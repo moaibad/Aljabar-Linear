44298e781127c78df42492c577aa704e4c44304e
moaibad <fathulibad.412@gmail.com>
2022-09-28 21:47:39 +0700

    update determinant module

--- main.c
+++ main.c
@@ -220,45 +220,39 @@ void inverse(){
 }
 
 //For calculating Determinant of the Matrix
-float determinant(float a[25][25],float k)
-{
-  float s=1,det=0,b[25][25];
-  int i,j,m,n,c;
-  if (k==1)
-    {
-     return (a[0][0]);
-    }
-  else
-    {
-     det=0;
-     for (c=0;c<k;c++)
-       {
-        m=0;
-        n=0;
-        for (i=0;i<k;i++)
-          {
-            for (j=0;j<k;j++)
-              {
-                b[i][j]=0;
-                if (i != 0 && j != c)
-                 {
-                   b[m][n]=a[i][j];
-                   if (n<(k-2))
-                    n++;
-                   else
-                    {
-                     n=0;
-                     m++;
-                     }
-                   }
-               }
-             }
-          det=det + s * (a[0][c] * determinant(b,k-1));
-          s=-1 * s;
-          }
-    }
-
-    return (det);
+float determinant(float a[25][25],float k){
+	float s=1,det=0,b[25][25];
+	int i,j,m,n,c;
+	
+	if (k==1){
+		return (a[0][0]);
+	}
+	else{
+		det=0;
+		for (c=0;c<k;c++){
+			m=0;
+			n=0;
+			for (i=0;i<k;i++){
+				for (j=0;j<k;j++){
+					b[i][j]=0;
+					if (i != 0 && j != c){
+						b[m][n]=a[i][j];
+						
+						if (n<(k-2)){
+							n++;	
+						}
+						else{
+							n=0;
+							m++;
+						}
+					}
+				}
+			}
+			det=det + s * (a[0][c] * determinant(b,k-1));
+			s=-1 * s;
+		}
+	}
+	return (det);
 }
 
 void cofactor(float num[25][25],float f){
